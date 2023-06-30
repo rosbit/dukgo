2e1fb07da18b2da1cda41b7d1b37a1641966b931
Rosbit Xu <me@rosbit.cn>
2023-06-30 14:19:05 +0800

    re-implements modSearch with native golang function

--- mod-search.go
+++ mod-search.go
@@ -1,42 +1,29 @@
 package djs
 
 // #include "duktape.h"
-// extern duk_ret_t readFile(duk_context *ctx);
+// extern duk_ret_t modSearch(duk_context *ctx);
 // static const char *getCString(duk_context *ctx, duk_idx_t idx);
-// static duk_int_t pEval(duk_context *ctx, const char *src, duk_size_t len);
 import "C"
 import (
+	"strings"
 	"fmt"
 	"os"
 	"path"
 )
 
-var (
-	modSearch_impl = `
-	Duktape.modSearch = function (id) {
-		/* readFile() reads a file from disk, and returns a string or undefined.
-		 * 'id' is in resolved canonical form so it only contains terms and
-		 * slashes, and no '.' or '..' terms.
-		 */
-		var name;
-		if (id.endsWith('.js')) {
-			name = id;
-		} else {
-			name = id + '.js';
-		}
-
-		var res = readFile(name);
-		if (typeof res === 'string') {
-			return res;
-		}
-
-		throw new Error('module not found: ' + id);
-	}`
-)
-
-//export readFile
-func readFile(ctx *C.duk_context) C.duk_ret_t {
+//export modSearch
+func modSearch(ctx *C.duk_context) C.duk_ret_t {
+	/* Nargs was given as 4 and we get the following stack arguments:
+	 *   index 0: id
+	 *   index 1: require
+	 *   index 2: exports
+	 *   index 3: module
+	 */
 	modPath := C.GoString(C.getCString(ctx, 0))
+	if !strings.HasSuffix(modPath, ".js") {
+		modPath = fmt.Sprintf("%s.js", modPath)
+	}
+	C.duk_pop_n(ctx, 4)
 
 	absModPath := toAbsPath(exePath, modPath)
 	b, err := os.ReadFile(absModPath)
@@ -53,34 +40,27 @@ func readFile(ctx *C.duk_context) C.duk_ret_t {
 	return 1;
 }
 
-func initGlobalFuncs(ctx *C.duk_context) {
-	C.duk_push_global_object(ctx)
-	setGlobalFunction(ctx, "readFile", (C.duk_c_function)(C.readFile), 1)
-	C.duk_pop(ctx)
-}
-
-func setGlobalFunction(ctx *C.duk_context, funcName string, fn C.duk_c_function, nargs int) {
+func setObjFunction(ctx *C.duk_context, funcName string, fn C.duk_c_function, nargs int) {
 	var cFuncName *C.char
 	var funcNameLen C.int
 	getStrPtrLen(&funcName, &cFuncName, &funcNameLen)
 
-	// [ global ]
-	C.duk_push_lstring(ctx, cFuncName, C.size_t(funcNameLen))  // [ global, funcName ]
-	C.duk_push_c_function(ctx, fn, C.duk_idx_t(nargs)) // [ global, funcName, fn ]
-	C.duk_put_prop(ctx, -3)  // [ global ] with global[funcName]=fn
+	// [ obj ]
+	C.duk_push_lstring(ctx, cFuncName, C.size_t(funcNameLen))  // [ obj funcName ]
+	C.duk_push_c_function(ctx, fn, C.duk_idx_t(nargs)) // [ obj funcName fn ]
+	C.duk_put_prop(ctx, -3)  // [ obj ] with obj[funcName]=fn
 }
 
-func set_modSearch(ctx *C.duk_context) {
-	var impl *C.char
+func setModSearch(ctx *C.duk_context) {
+	duktape := "Duktape"
+	var cDuktape *C.char
 	var length C.int
-	getStrPtrLen(&modSearch_impl, &impl, &length)
-	if C.pEval(ctx, impl, C.size_t(length)) != 0 {
-		estr := C.getCString(ctx, -1)
-		fmt.Fprintf(os.Stderr, "failed to set modSearch: %s", C.GoString(estr))
-	}
+	getStrPtrLen(&duktape, &cDuktape, &length)
+
+	C.duk_get_global_lstring(ctx, cDuktape, C.size_t(length))
+	setObjFunction(ctx, "modSearch", (C.duk_c_function)(C.modSearch), 4)
 	C.duk_pop(ctx)
 }
-
 
 func getExecWD() (string, error) {
 	exePath, err := os.Executable()
